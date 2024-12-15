5f24ff619be001f636946025b448c26626a1cf08
Kiran Anand <liteninkiran@hotmail.com>
2024-12-15 17:53:08 +0000

    Lesson 06

--- src/App.js
+++ src/App.js
@@ -10,11 +10,15 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Faq from './pages/help/Faq';
 import Contact from './pages/help/Contact';
+import Careers from './pages/careers/Careers';
 import NotFound from './pages/NotFound';
 
 // Layouts
 import RootLayout from './layouts/RootLayout';
 import HelpLayout from './layouts/HelpLayout';
+import CareersLayout from './layouts/CareersLayout';
+
+import { careersLoader } from './pages/careers/Careers';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -25,8 +29,17 @@ const router = createBrowserRouter(
         <Route path='faq' element={<Faq />} />
         <Route path='contact' element={<Contact />} />
       </Route>
+      <Route path="careers" element={<CareersLayout />}>
+        <Route
+          index
+          element={<Careers />}
+          loader={careersLoader}
+        />
+      </Route>
+
       <Route path='*' element={<NotFound />} />
     </Route>
+
   )
 )
 
